6f5c1171cf961f0a88fdc7131157fc5aba6842fe
Lamont Lucas <lamont@fastrobot.com>
2015-09-25 09:14:31 -0700

    updates to use an array internally. Works as well.
    Working, running every 2 minutes out of cron. Yes, I checked in our auth codes but I don't worry about that as they're firewalled away.

--- muniPoller.rb
+++ muniPoller.rb
@@ -4,17 +4,35 @@
 #  N inbound "Sunset Tunnel East Portal"
 #  37 outbound to twin peaks
 #  24 outbound to castro
+#  24 inbound as well
 
 require 'rubygems'
 require 'muni'
+require 'httparty'
+require 'json'
 
-stops = {
-  'N' => { dir: 'inbound', stop: 'Sunset Tunnel East Portal' },
-  '37' => { dir: 'outbound', stop: '14th St & Castro St' },
-  '24' => { dir: 'outbound', stop: 'Castro St & 15th St' }
-}
+url = 'https://data-engine-192-168-178-47.jutdata.io:3110/api/v1/import/webhook/?space=muni&data_source=nextbus_webhook&apikey=2Lols63-U3ze&field_connector=webhook&field_data_source=nextbus_webhook'
 
-stops.each do |route, details|
-  puts route + ' ' + details[:dir] + ' in ' +
-    Muni::Route.find(route).direction_at(details[:dir]).stop_at(details[:stop]).predictions.first.minutes
+stops = [
+  ['N','inbound','Sunset Tunnel East Portal' ],
+  ['37', 'outbound', '14th St & Castro St' ],
+  ['24', 'outbound', 'Castro St & 15th St' ],
+  ['24', 'inbound', 'Castro St & 15th St' ]
+]
+
+predictions = {}
+
+stops.each do |entry|
+  r,d,s = entry
+  predictions[entry.join('-').gsub(' ','_')] =
+      Muni::Route.find(r).direction_at(d).stop_at(s).predictions.first.minutes.to_i
 end
+
+# puts predictions.to_json
+
+response = HTTParty.post(url, body: predictions.to_json, :headers => { 'Content-Type' => 'application/json' } )
+
+# puts response.body
+# puts response.message
+# puts response.code
+# puts response.headers.inspect
